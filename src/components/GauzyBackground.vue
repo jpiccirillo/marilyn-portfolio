b3b1a9b0d946632678dbaf6a5b0e4b9c1f82bdf5
jeffrey <piccirilloj1@gmail.com>
2022-11-15 21:17:36 -0600

    Set up GauzyBackground component to own the hazy white bg, w photo of choice

---
+++ src/components/GauzyBackground.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="page-container">
+    <div class="background-container">
+      <div class="background" :style="style"></div>
+      <div class="overlay"></div>
+      <div class="card-container">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GauzyBackground',
+  props: ['background'],
+  computed: {
+    style() {
+      return {
+        background: `url(${this.background}) no-repeat center center fixed`,
+      }
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.page-container,
+.background-container,
+.card-container {
+  height: 100%;
+  width: 100%;
+}
+
+.background {
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+.background-container {
+  display: grid;
+}
+
+.overlay {
+  background-color: white;
+  opacity: 0.3;
+}
+
+.overlay,
+.background,
+.card-container {
+  grid-row-start: 1;
+  grid-column-start: 1;
+}
+
+.card-container {
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  max-width: 100vw;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="hello"><div class="page">Home</div></div>
+  <GauzyBackground :background="require('../assets/blurrySea.webp')">
+    <div class="card">
+      <q class="quote">Copy of page here</q>
+    </div>
+  </GauzyBackground>
 </template>
 
 <script>
+import GauzyBackground from '../components/GauzyBackground.vue'
+
 export default {
   name: 'Home',
   props: {
     msg: String,
   },
+  components: { GauzyBackground },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.card {
+  max-width: 800px;
+  height: 500px;
+  background-color: var(--navbar-background);
+  opacity: 0.9;
+  border-radius: 3px;
+  margin: 30px;
+  padding: 30px;
+  box-sizing: border-box;
+}
+
+.quote {
+  font-family: Tahoma;
+}
+</style>
